42183859c3c18bb0310107fdd22b6b3d1efb863f
Sri Hari <sriharir1711@gmail.com>
2026-06-15 12:36:35 +0530

    fix: Postgres URL driver mapping and add DB migration to Railway start command

--- core/database.py
+++ core/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from core.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=False)
+engine = create_async_engine(settings.get_database_url, echo=False)
 SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 async def get_db():
